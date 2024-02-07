e0816e98cdccc516f9af298b8c45dc51e7da9d24
zach <zstlouis7@gmail.com>
2024-02-07 15:05:11 -0800

    init

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+
+if(1==2){
+    console.log("This is some text ")
+}else{
+    console.log("this is some other text")
+}
+
+
+let str = "Some text";
+console.log(str.substring(2,6))
+
+function add(x, y){
+    return x + y;
+}
+console.log(add(2,3));
